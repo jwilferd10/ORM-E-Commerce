000cb71970cfd8a0437f5c77d0e1f39b703b6efd
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-01-23 20:15:15 -0800

    Feat: Fleshed out index.js and worked on association

--- models/index.js
+++ models/index.js
@@ -4,12 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsto Category, as a category can have multiple products but a product can only belong to one category.
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+});
+// Category hasmany Product models.
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
+// Products belongToMany Tags (through ProductTag, allow products to have multiple tags and tags to have many products.)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
 
-// Categories have many Products
+});
+// Tags belongToMany Products (through ProductTag) allow products to have multiple tags and tags to have many products.
+Tag.belongsToMany(Product, {
+  through: ProductTag,
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+});
 
 module.exports = { Product, Category, Tag, ProductTag, };
