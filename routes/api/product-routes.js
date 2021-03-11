32224e29169fa778c6b5a151b908444d9f46e88d
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-10 21:35:52 -0800

    Feat: Product findAll works, issue with the error statement

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,21 +10,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   Product.findAll (
     {
-      // include: [
-      //   {
-      //     model: Category,
-      //     attributes: ['category_name']
-      //   },
-      //   {
-      //     model: Tag,
-      //     attributes: ['tag_name']
-      //   }
-      // ]
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
     }
   )
   .then(dbProductData => res.json(dbProductData))
-  .catch (err);
+  .catch(err => {
+    console.log(err);
     res.status(500).json(err);
+  });
 });
 
 //==================//
